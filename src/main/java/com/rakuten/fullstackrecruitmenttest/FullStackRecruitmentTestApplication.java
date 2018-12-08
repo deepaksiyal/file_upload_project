07438409c38d50b78496635df27bc575ca25082e
deepaksiyal <deepak1.siyal@paytm.com>
2018-12-08 21:29:46 +0530

    File Uploading Project

---
+++ src/main/java/com/rakuten/fullstackrecruitmenttest/FullStackRecruitmentTestApplication.java
@@ -0,0 +1,18 @@
+package com.rakuten.fullstackrecruitmenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.rakuten.fullstackrecruitmenttest.property.FileStorageProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class FullStackRecruitmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackRecruitmentTestApplication.class, args);
+	}
+}
